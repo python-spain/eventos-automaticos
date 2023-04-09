a0565e6c290202170d5067e3561cfe282258e58a
alexgmin <alexgmin@gmail.com>
2023-04-09 12:10:38 +0200

    main.py Basic structure

--- main.py
+++ main.py
@@ -2,9 +2,26 @@
 """
 Publish meetup events to Telegram.
 """
-import os
+import tomllib
 
-meetup_secret = os.environ["MEETUP_SECRET"]
+meetup_client_secret = os.environ['MEETUP_SECRET']
+meetup_client_key = os.environ['MEETUP_KEY']
+meetup_member_id = os.environ['MEMBER_MEMBER_ID']
+
+
+def get_events(community_slug):
+    pass
+
+
+def process_events():
+    with open('communities.toml', 'rb') as f:
+        for community in tomllib.load(f)['communities']:
+            event = get_events(community)
+            publish_to_telegram(event)
+
+
+def publish_to_telegram(event):
+    pass
 
 
 def main():
