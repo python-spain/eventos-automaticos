4eef8d462ef27db0bac9141d0a405a5ab1b9af49
Juan Antonio Oyagüe Cobelas <joyague@full-on-net.com>
2023-04-09 11:13:14 +0200

    main.py with secret.

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Publish meetup events to Telegram.
+"""
+import os
+
+meetup_secret = os.environ.get("MEETUP_SECRET")
+
+
+def main():
+    print(f"Hello Meetup! (secret {meetup_secret[:2]}{meetup_secret[2:] * '*'})")
+
+
+if __name__ == '__main__':
+    main()
