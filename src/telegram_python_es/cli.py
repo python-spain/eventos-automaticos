cafeeaf22a9c1e6f77bc3dfcd2488d1733db6f06
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-05-01 10:24:58 +0200

    Produce 1 JSON per event

--- src/telegram_python_es/cli.py
+++ src/telegram_python_es/cli.py
@@ -1,22 +1,43 @@
+import json
 import tomllib
-from pprint import pprint
+from pathlib import Path
 
 import click
+from dateutil.parser import parse as parse_date
 
 from .graphql_query import collect_upcoming_events
+
+
+def event_name_from_data(event):
+    event_start_time = parse_date(event["dateTime"])
+    return (
+        f"{event_start_time:%Y-%m-%d}_{event['title'].replace(' ', '-')}_{event['id']}"
+    )
 
 
 @click.command()
 @click.option(
     "--communities", "-c", "communities_path", type=click.Path(), required=True
 )
-def cli(communities_path):
+@click.option("--destination-dirname", "-d", type=click.Path(), default="_events")
+def cli(communities_path, destination_dirname):
     with open(communities_path, "rb") as f:
         communities_data = tomllib.load(f)
     communities = communities_data["communities"]
 
     upcoming_events = collect_upcoming_events(communities)
-    pprint(upcoming_events)
+
+    if not (destination_dir := Path(destination_dirname)).is_dir():
+        destination_dir.mkdir()
+
+    for community_slug, events in upcoming_events.items():
+        if not (community_dir := (destination_dir / community_slug)).is_dir():
+            community_dir.mkdir()
+
+        for event_data in events:
+            event = event_data["data"]["event"]
+            with open(community_dir / f"{event_name_from_data(event)}.json", "w") as fh:
+                json.dump(event, fh, indent=2)
 
 
 if __name__ == "__main__":
